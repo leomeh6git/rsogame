e2de2760a5004d576e9ead7a3703dcf53932fd65
leomeh6git <89913888+leomeh6git@users.noreply.github.com>
2024-08-22 11:13:43 +0000

    frsrt

---
+++ server.js
@@ -0,0 +1,21 @@
+const WebSocket = require('ws');
+const server = new WebSocket.Server({ port: 7890 });
+
+let cipherPhrase = 'No cipher phrase set.';
+
+server.on('connection', ws => {
+    // Send the current cipher phrase to the new client
+    ws.send(cipherPhrase);
+
+    // Broadcast any new cipher phrase to all clients
+    ws.on('message', message => {
+        cipherPhrase = message;
+        server.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(cipherPhrase);
+            }
+        });
+    });
+});
+
+console.log('WebSocket server is running on ws://localhost:8080');
